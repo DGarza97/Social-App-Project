bbb7029eab6e6a6364ba83b27462a66742fd5eca
Danny <147435518+DGarza97@users.noreply.github.com>
2025-12-14 01:36:37 -0600

    Added ability to post and add posts to database

--- feed/urls.py
+++ feed/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.feed_page, name='feed'),
-    path('myprojects/', views.myprojects, name='myprojects')
+    path('myprojects/', views.myprojects, name='myprojects'),
 ]
+
--- feed/views.py
+++ feed/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+from .models import Post
 
 def feed_page(request):
-    return render(request, 'feed/feed.html')
+    if request.method == "POST":
+        content = request.POST.get("content")
+
+        if content:
+            Post.objects.create(
+                author=request.user,
+                content=content
+            )
+
+    posts = Post.objects.order_by("-created_at")
+
+    return render(request, "feed/feed.html", {
+        "posts": posts
+    })
 
 def myprojects(request):
     return render(request, 'myprojects/myprojects.html')
