bd343056aad6a5ef07b2f5249248afa3d07ff661
Danny <147435518+DGarza97@users.noreply.github.com>
2025-12-17 18:37:52 -0600

    Fixed issue with groups

--- groups/views.py
+++ groups/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Group
 
+
 @login_required
 def groups_page(request):
+    # CREATE GROUP
     if request.method == "POST":
         Group.objects.create(
-            name=request.POST["name"],
-            description=request.POST["description"],
+            name=request.POST.get("name"),
+            description=request.POST.get("description"),
             owner=request.user
         )
         return redirect("groups")
 
+    # LIST GROUPS
     groups = Group.objects.all()
     return render(request, "groups/groups.html", {
         "groups": groups
@@ -20,10 +23,8 @@ def groups_page(request):
 
 @login_required
 def join_group(request, group_id):
-    group = Group.objects.create(
-        name=request.POST["name"],
-        description=request.POST["description"],
-        owner=request.user
-    )
-    group.members.add(request.user)
+    if request.method == "POST":
+        group = get_object_or_404(Group, id=group_id)
+        group.members.add(request.user)
+
     return redirect("groups")
